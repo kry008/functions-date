96a74ecfff191d0e15e48471910bfbfc0dcb2e3d
kry008 <pkrzysaaaa@gmail.com>
2021-10-31 17:29:06 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,112 @@
+# Date Functions
+GitHub:
+![GitHub repo size](https://img.shields.io/github/repo-size/kry008/functions_date) ![GitHub all releases](https://img.shields.io/github/downloads/kry008/functions_date/total) ![GitHub issues](https://img.shields.io/github/issues/kry008/functions_date) ![GitHub closed issues](https://img.shields.io/github/issues-closed-raw/kry008/functions_date) ![GitHub pull requests](https://img.shields.io/github/issues-pr-raw/kry008/functions_date) ![GitHub package.json version](https://img.shields.io/github/package-json/v/kry008/functions_date)
+
+NPM:
+![NPM](https://img.shields.io/npm/l/date_fuctions) ![npm](https://img.shields.io/npm/dw/functions_date) ![npm](https://img.shields.io/npm/v/functions_date)
+## Installation
+```console
+npm i functions_date
+```
+## Importing package
+```js
+var df = require("functions_date")
+```
+or
+```js
+import { name_of_fuctions } from "functions_date";
+```
+## Usage
+### require
+```js
+var variable = df.function()
+```
+### import
+```js
+import {function_name1, function_name2, ... } from  "functions_date";
+```
+## Functions
+```js
+//changing language, available EN - 0, PL - 1, DE - 2 
+language(language_code)
+//Today's name according to the set language e.g. Monday
+D()
+//Day name from object according to the set language e.g. Monday
+D_given_date(date_object)
+//Name of day from number (1 - Monday, 7 - Sunday) according to the set language
+D_given_number(day_number_from_zero)
+//The name of the current month in the set language.
+M() 
+//The name of the month from the Date object in the set language.
+M_given_date(date_object)
+//Month name as an argument of the month number, result e.g. 1 - January
+M_given_number(month_number)
+//Shows today's date in the format Day (D) Month (M) Year (Y), and optionally you can enter a character between DD MM YYYY as a function value, by default "-"
+DDMMYYYY(char = "-")
+MMDDYYYY(char = "-")
+//Shows the date from the Date object in the Day (D) Month (M) Year (Y) format, and optionally you can enter a character between DD MM YYYY as a function value, by default "-"
+DDMMYYYY_given_date(date, char = "-")
+MMDDYYYY_given_date(date, char = "-")
+//Shows actual time (24h), HH MM (SS), and optionally you can enter a character between HH MM (SS) as a function value, by default ":"
+HHMM24(char=":")
+HHMMSS24(char=":")
+//Shows time (24h) from Date object, HH MM (SS), and optionally you can enter a character between HH MM (and SS) as a function value, by default ":"
+HHMM24_given_date(date, char=":")
+HHMMSS24_given_date(date, char=":")
+//Shows actual time in the 12h pattern, HH MM (and SS) optionally you can enter a sign between HH and MM (and SS) and the abbreviation AM and PM
+HHMM12(char=":", am="AM", pm="PM")
+HHMMSS12(char=":", am="AM", pm="PM")
+//Shows Date object time in the 12h pattern, optionally you can enter a sign between HH and MM (and SS) and the abbreviation AM and PM
+HHMM12_given_date(date, char=":", am="AM", pm="PM")
+HHMMSS12_given_date(date, char=":", am="AM", pm="PM")
+//Shows miliseconds since January 1, 1970 till today
+TimeMS()
+//Shows miliseconds since January 1, 1970 till Date object
+TimeMS_given_date(date)
+//Now miliscends
+MS()
+//Miliseconds from Date object
+MS_given_date(date)
+//Now seconds
+SS()
+//Seconds from Date object
+SS_given_date(date)
+//Now Minutes
+MM()
+//Minutes from Date object
+MM_given_date(date)
+//Now Hours
+HH()
+//Hours from Date object
+HH_given_date(date)
+//Now Day number 1-7
+Day()
+//Day from Date object number 1-7
+Day_given_date(date)
+//Now Month number 1-12
+Month()
+//Month from Date object number 1-7
+Month_given_date(date)
+//Now Year
+Year()
+//Year from Date object
+Year_given_date(date)
+//Prints the number and name of the season in a fixed language, and has the option to accept true / false or refer to the northern hemisphere
+seasons(northern_hemisphere = true)
+//As above, but with object Date
+seasons_given_date(date, northern_hemisphere = true)
+//SQL timestamp
+timestampSQL()
+//SQL timestamp from Date object
+timestampSQL_given_date(date)
+```
+## Extra
+### Random numbers
+```js
+r0_9() //0-9
+r0_10() //0-10
+r1_10() //1-10
+r0_99() //0-99
+r0_100() //0-00
+r1_100() //1-100
+```
---
+++ index.js
@@ -0,0 +1,541 @@
+const Data = new Date();
+var language = 1;//0 - EN, 1 - PL, 2 - DE
+var debug = false;
+
+var dayOfWeek = []
+dayOfWeek[0] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+dayOfWeek[1] = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"];
+dayOfWeek[2] = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+var monthOfWeek = []
+monthOfWeek[0] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+monthOfWeek[1] = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
+monthOfWeek[2] = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+var seasons_tab = [];
+seasons_tab[0] = ["Spring", "Summer", "Autumn", "Winter"]
+seasons_tab[1] = ["Wiosna", "Lato", "Jesień", "Zima"]
+seasons_tab[2] = ["Frühling", "Sommer", "Herbst", "Winter"]
+exports.debug = function(debug = false) {
+    console.log(Data);
+    console.log(language);
+    console.log(debug);
+    debug = this.debug
+}
+exports.language = function(lang)
+{
+    console.log(`Language = ${lang}`)
+    //language = lang;
+    switch (lang) {
+        case "en":
+        case "0":
+        case 0:
+            language = 0;
+            if (debug) {
+                console.log("EN")
+            }
+            break;
+        case "pl":
+        case "1":
+        case 1:
+            language = 1;
+            if (debug) {
+                console.log("PL")
+            }
+            break;
+        case "de":
+        case "2":
+        case 2:
+            language = 2;
+            if (debug) {
+                console.log("DE")
+            }
+            break;
+    
+        default:
+            console.error("Language unsupported, default EN")
+            language = 0;
+            break;
+    }
+}
+exports.D = function() //today Day Name
+{
+    if (debug) {
+        console.log(language + " " + (Data.getDay()))
+    }
+    return dayOfWeek[language][Data.getDay()]
+}
+exports.D_given_date = function(date) //from object Date
+{
+    if (debug) {
+        console.log(language + " " + date.getDay())
+    }
+    return dayOfWeek[language][date.getDay()]
+}
+exports.D_given_number = function(day_number) //Day of week, from 1
+{
+    if (day_number == 7) {
+        day_number = 0
+    }
+    console.log(language + " " + day_number)
+    return dayOfWeek[language][day_number]
+}
+exports.M = function() //today month Name
+{
+    if (debug) {
+        console.log(language + " " + (Data.getMonth()))
+    }
+    return monthOfWeek[language][Data.getMonth()]
+}
+exports.M_given_date = function(date) //from object Date
+{
+    console.log(language + " " + (date.getMonth()))
+    return monthOfWeek[language][date.getMonth()]
+}
+exports.M_given_number = function(month_number) //month, from 1
+{
+    month_number = month_number - 1;
+    console.log(language + " " + month_number)
+    return monthOfWeek[language][month_number]
+}
+exports.DDMMYYYY = function(char = "-")//Char between dayXmonthXyear
+{
+    return Data.getDate() + char + (Data.getMonth() + 1) + char + Data.getFullYear()
+}
+exports.MMDDYYYY = function(char = "-")//Char between monthXdayXyear
+{
+    return (Data.getMonth() + 1) + char + Data.getDate() + char + Data.getFullYear()
+}
+exports.DDMMYYYY_given_date = function(date, char = "-")//Char between dayXmonthXyear
+{
+    return date.getDate() + char + (date.getMonth() + 1) + char + date.getFullYear()
+}
+exports.MMDDYYYY_given_date = function(date, char = "-")//Char between monthXdayXyear
+{
+    return (date.getMonth() + 1) + char + date.getDate() + char + date.getFullYear()
+}
+exports.HHMM24 = function(char=":")
+{
+    var hours = Data.getHours();
+    var minutes = Data.getMinutes();
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+
+    return hours + char + minutes
+}
+exports.HHMM12 = function(char=":", am="AM", pm="PM")
+{
+    var hours = Data.getHours();
+    var minutes = Data.getMinutes();
+    var ampm = hours >= 12 ? pm : am;
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    return hours + char + minutes + " " + ampm
+}
+exports.HHMM24_given_date = function(date, char=":")
+{
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+
+    return hours + char + minutes
+}
+exports.HHMM12_given_date = function(date, char=":", am="AM", pm="PM")
+{
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? pm : am;
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    return hours + char + minutes + " " + ampm
+}
+exports.HHMMSS24 = function(char=":")
+{
+    var hours = Data.getHours();
+    var minutes = Data.getMinutes();
+    var seconds = Data.getSeconds();
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    seconds = seconds < 10 ? '0'+seconds : seconds;
+
+    return hours + char + minutes + char + seconds
+}
+exports.HHMMSS12 = function(char=":", am="AM", pm="PM")
+{
+    var hours = Data.getHours();
+    var minutes = Data.getMinutes();
+    var seconds = Data.getSeconds();
+    var ampm = hours >= 12 ? pm : am;
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    seconds = seconds < 10 ? '0'+seconds : seconds;
+    return hours + char + minutes + char + seconds + " " + ampm
+}
+exports.HHMMSS24_given_date = function(date, char=":")
+{
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    seconds = seconds < 10 ? '0'+seconds : seconds;
+
+    return hours + char + minutes + char + seconds
+}
+exports.HHMMSS12_given_date = function(date, char=":", am="AM", pm="PM")
+{
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var ampm = hours >= 12 ? pm : am;
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    seconds = seconds < 10 ? '0'+seconds : seconds;
+    return hours + char + minutes + char + seconds + " " + ampm
+}
+exports.TimeMS = function()
+{
+    return Data.getTime()
+}
+exports.TimeMS_given_date = function(date)
+{
+    return date.getTime()
+}
+exports.MS = function()
+{
+    return Data.getMilliseconds()
+}
+exports.MS_given_date = function(date)
+{
+    return date.getMilliseconds()
+}
+exports.SS = function()
+{
+    return Data.getSeconds()
+}
+exports.SS_given_date = function(date)
+{
+    return date.getSeconds()
+}
+exports.MM = function()
+{
+    return Data.getMinutes()
+}
+exports.MM_given_date = function(date)
+{
+    return date.getMinutes()
+}
+exports.HH = function()
+{
+    return Data.getHours()
+}
+exports.HH_given_date = function(date)
+{
+    return date.getHours()
+}
+exports.Day = function()
+{
+    if(Data.getDate() == 0)
+    {
+        return 7
+    }
+    return Data.getDate()
+}
+exports.Day_given_date = function(date)
+{
+    if(date.getDate() == 0)
+    {
+        return 7
+    }
+    return date.getDate()
+}
+exports.Month = function()
+{
+    return (Data.getMonth() + 1)
+}
+exports.Month_given_date = function(date)
+{
+    return (date.getMonth() + 1)
+}
+exports.Year = function()
+{
+    return Data.getFullYear()
+}
+exports.Year_given_date = function(date)
+{
+    return date.getFullYear()
+}
+exports.seasons = function(northern_hemisphere = true)
+{
+    var month = Data.getMonth() +1;
+    var day = Data.getDay();
+    if(northern_hemisphere)
+    {
+        if (month == 1 || month == 2) {
+            return {seasons_nr: 4, seasons: seasons_tab[language][3]}
+        }
+        if (month == 3) {
+            if(day < 21)
+            {
+                return {seasons_nr: 4, seasons: seasons_tab[language][3]}
+            }
+            else
+            {
+                return {seasons_nr: 1, seasons: seasons_tab[language][0]}
+            }
+        }
+        if (month == 4 || month == 5) {
+            return {seasons_nr: 1, seasons: seasons_tab[language][0]}
+        }
+        if (month == 6) {
+            if(day < 22)
+            {
+                return {seasons_nr: 1, seasons: seasons_tab[language][0]}
+            }
+            else
+            {
+                return {seasons_nr: 2, seasons: seasons_tab[language][1]}
+            }
+        }
+        if (month == 7 || month == 8) {
+            return {seasons_nr: 2, seasons: seasons_tab[language][1]}
+        }
+        if (month == 9) {
+            if(day < 23)
+            {
+                return {seasons_nr: 2, seasons: seasons_tab[language][1]}
+            }
+            else
+            {
+                return {seasons_nr: 3, seasons: seasons_tab[language][2]}
+            }
+        }
+        if (month == 10 || month == 11) {
+            return {seasons_nr: 3, seasons: seasons_tab[language][2]}
+        }
+        if (month == 12) {
+            if(day < 22)
+            {
+                return {seasons_nr: 3, seasons: seasons_tab[language][2]}
+            }
+            else
+            {
+                return {seasons_nr: 4, seasons: seasons_tab[language][3]}
+            }
+        }
+    }
+    else
+    {
+        if (month == 1 || month == 2) {
+            return {seasons_nr: 2, seasons: seasons_tab[language][1]}
+        }
+        if (month == 3) {
+            if(day < 21)
+            {
+                return {seasons_nr: 2, seasons: seasons_tab[language][1]}
+            }
+            else
+            {
+                return {seasons_nr: 3, seasons: seasons_tab[language][2]}
+            }
+        }
+        if (month == 4 || month == 5) {
+            return {seasons_nr: 3, seasons: seasons_tab[language][2]}
+        }
+        if (month == 6) {
+            if(day < 22)
+            {
+                return {seasons_nr: 3, seasons: seasons_tab[language][2]}
+            }
+            else
+            {
+                return {seasons_nr: 4, seasons: seasons_tab[language][3]}
+            }
+        }
+        if (month == 7 || month == 8) {
+            return {seasons_nr: 4, seasons: seasons_tab[language][3]}
+        }
+        if (month == 9) {
+            if(day < 23)
+            {
+                return {seasons_nr: 4, seasons: seasons_tab[language][3]}
+            }
+            else
+            {
+                return {seasons_nr: 1, seasons: seasons_tab[language][0]}
+            }
+        }
+        if (month == 10 || month == 11) {
+            return {seasons_nr: 1, seasons: seasons_tab[language][0]}
+        }
+        if (month == 12) {
+            if(day < 22)
+            {
+                return {seasons_nr: 1, seasons: seasons_tab[language][0]}
+            }
+            else
+            {
+                return {seasons_nr: 2, seasons: seasons_tab[language][1]}
+            }
+        }
+    }
+}
+exports.seasons_given_date = function(date, northern_hemisphere = true)
+{
+    console.log("AAAA")
+    var month = date.getMonth() +1;
+    var day = date.getDay();
+    if(northern_hemisphere)
+    {
+        if (month == 1 || month == 2) {
+            return {seasons_nr: 4, seasons: seasons_tab[language][3]}
+        }
+        if (month == 3) {
+            if(day < 21)
+            {
+                return {seasons_nr: 4, seasons: seasons_tab[language][3]}
+            }
+            else
+            {
+                return {seasons_nr: 1, seasons: seasons_tab[language][0]}
+            }
+        }
+        if (month == 4 || month == 5) {
+            return {seasons_nr: 1, seasons: seasons_tab[language][0]}
+        }
+        if (month == 6) {
+            if(day < 22)
+            {
+                return {seasons_nr: 1, seasons: seasons_tab[language][0]}
+            }
+            else
+            {
+                return {seasons_nr: 2, seasons: seasons_tab[language][1]}
+            }
+        }
+        if (month == 7 || month == 8) {
+            return {seasons_nr: 2, seasons: seasons_tab[language][1]}
+        }
+        if (month == 9) {
+            if(day < 23)
+            {
+                return {seasons_nr: 2, seasons: seasons_tab[language][1]}
+            }
+            else
+            {
+                return {seasons_nr: 3, seasons: seasons_tab[language][2]}
+            }
+        }
+        if (month == 10 || month == 11) {
+            return {seasons_nr: 3, seasons: seasons_tab[language][2]}
+        }
+        if (month == 12) {
+            if(day < 22)
+            {
+                return {seasons_nr: 3, seasons: seasons_tab[language][2]}
+            }
+            else
+            {
+                return {seasons_nr: 4, seasons: seasons_tab[language][3]}
+            }
+        }
+    }
+    else
+    {
+        if (month == 1 || month == 2) {
+            return {seasons_nr: 2, seasons: seasons_tab[language][1]}
+        }
+        if (month == 3) {
+            if(day < 21)
+            {
+                return {seasons_nr: 2, seasons: seasons_tab[language][1]}
+            }
+            else
+            {
+                return {seasons_nr: 3, seasons: seasons_tab[language][2]}
+            }
+        }
+        if (month == 4 || month == 5) {
+            return {seasons_nr: 3, seasons: seasons_tab[language][2]}
+        }
+        if (month == 6) {
+            if(day < 22)
+            {
+                return {seasons_nr: 3, seasons: seasons_tab[language][2]}
+            }
+            else
+            {
+                return {seasons_nr: 4, seasons: seasons_tab[language][3]}
+            }
+        }
+        if (month == 7 || month == 8) {
+            return {seasons_nr: 4, seasons: seasons_tab[language][3]}
+        }
+        if (month == 9) {
+            if(day < 23)
+            {
+                return {seasons_nr: 4, seasons: seasons_tab[language][3]}
+            }
+            else
+            {
+                return {seasons_nr: 1, seasons: seasons_tab[language][0]}
+            }
+        }
+        if (month == 10 || month == 11) {
+            return {seasons_nr: 1, seasons: seasons_tab[language][0]}
+        }
+        if (month == 12) {
+            if(day < 22)
+            {
+                return {seasons_nr: 1, seasons: seasons_tab[language][0]}
+            }
+            else
+            {
+                return {seasons_nr: 2, seasons: seasons_tab[language][1]}
+            }
+        }
+    }
+}
+exports.timestampSQL = function()
+{
+    var timestamp = Data.getUTCFullYear() + '-' +
+    ('00' + (Data.getUTCMonth()+1)).slice(-2) + '-' +
+    ('00' + Data.getUTCDate()).slice(-2) + ' ' + 
+    ('00' + Data.getUTCHours()).slice(-2) + ':' + 
+    ('00' + Data.getUTCMinutes()).slice(-2) + ':' + 
+    ('00' + Data.getUTCSeconds()).slice(-2);
+    return (timestamp)
+}
+exports.timestampSQL_given_date = function(date)
+{
+    var timestamp = date.getUTCFullYear() + '-' +
+    ('00' + (date.getUTCMonth()+1)).slice(-2) + '-' +
+    ('00' + date.getUTCDate()).slice(-2) + ' ' + 
+    ('00' + date.getUTCHours()).slice(-2) + ':' + 
+    ('00' + date.getUTCMinutes()).slice(-2) + ':' + 
+    ('00' + date.getUTCSeconds()).slice(-2);
+    return (timestamp)
+}
+exports.r0_9 = function()
+{
+    return Math.floor(Math.random() * 10);
+}
+exports.r0_10 = function()
+{
+    return Math.floor(Math.random() * 11);
+}
+exports.r1_10 = function()
+{
+    return (Math.floor(Math.random() * 10) + 1);
+}
+exports.r0_99 = function()
+{
+    return Math.floor(Math.random() * 100);
+}
+exports.r0_100 = function()
+{
+    return Math.floor(Math.random() * 101);
+}
+exports.r1_100 = function()
+{
+    return (Math.floor(Math.random() * 100) + 1)
+}
